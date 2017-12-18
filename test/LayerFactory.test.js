1f6e69a8e3092cfb594e83069252a43d723ceea0
Shaun Lusk <slusk@accruent.com>
2017-12-17 23:41:12 -0600

    LayerFactoryTests

---
+++ test/LayerFactory.test.js
@@ -0,0 +1,53 @@
+describe("LayerFactory", function() {
+  describe("#constructor", function() {
+    it("should setup default layer type creator functions", function(done) {
+      var lf = new SL.LayerFactory();
+
+      assert(lf._registeredTypes !== null, "_registeredTypes was not created");
+      assert(SL.isFunction(lf._registeredTypes.GfxLayer), "_registeredTypes does not contain function for GfxLayer");
+      assert(SL.isFunction(lf._registeredTypes.BackgroundLayer), "_registeredTypes does not contain function for BackgroundLayer");
+      done();
+    });
+    it("should setup default layer type creator functions and those passed in", function(done) {
+      var lf = new SL.LayerFactory({
+        "BogusLayer":function() {}
+      });
+
+      assert(lf._registeredTypes !== null, "_registeredTypes was not created");
+      assert(SL.isFunction(lf._registeredTypes.GfxLayer), "_registeredTypes does not contain function for GfxLayer");
+      assert(SL.isFunction(lf._registeredTypes.BackgroundLayer), "_registeredTypes does not contain function for BackgroundLayer");
+      assert(SL.isFunction(lf._registeredTypes.BogusLayer), "_registeredTypes does not contain function for BogusLayer");
+      done();
+    });
+  });
+  describe("#getLayer", function() {
+    it("should create GfxLayer", function(done) {
+      var lf = new SL.LayerFactory();
+
+      var result = lf.getLayer(null, "GfxLayer", null);
+
+      assert(result !== null, "should have created layer");
+      assert(result instanceof SL.GfxLayer, "should have created GfxLayer type");
+      done();
+    });
+    it("should create BackgroundLayer", function(done) {
+      var lf = new SL.LayerFactory();
+
+      var result = lf.getLayer(null, "BackgroundLayer", null);
+
+      assert(result !== null, "should have created layer");
+      assert(result instanceof SL.BackgroundLayer, "should have created BackgroundLayer type");
+      done();
+    });
+    it("should create other layer", function(done) {
+      var lf = new SL.LayerFactory({
+        "BogusLayer":function() {return "fakeLayer";}
+      });
+
+      var result = lf.getLayer(null, "BogusLayer", null);
+
+      assert(result === "fakeLayer", "should have created fake layer");
+      done();
+    });
+  });
+});
